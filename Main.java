8435a8554dae5e39388a2b129696d25ade342b35
Noah <10984564+noah-frank@users.noreply.github.com>
2022-11-30 15:03:14 +0100

    Add printTwoDimArray method and test code

--- Main.java
+++ Main.java
@@ -8,9 +8,28 @@ public class Main{
 
         int heatmap [][] = generateHeatmap(world);
 
-        
+        printTwoDimArray(world);
+
 
     }
+
+
+    public static void printTwoDimArray(int [][] array){
+
+        System.out.println("");
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+
+
+    }
+
+
 
 
     public static int [][] generateHeatmap(int [][] world){
